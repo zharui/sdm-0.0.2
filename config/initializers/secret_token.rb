d15d2b59819c916a88b24164088197beca1f3dcc
zharui <zharui@semidata.com>
2014-02-19 17:26:25 +0800

    start merge fe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sdm002::Application.config.secret_key_base = '61c08409c76f6580843301b0a5ff29597fb7ceed881d5d3d60c1e0f6a38e547c5c451c336bd49437fcbe4046903f57dd526f6668173dbe2476dad0bfc57946fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sdm002::Application.config.session_store :cookie_store, key: '_sdm-0_0_2_session'
